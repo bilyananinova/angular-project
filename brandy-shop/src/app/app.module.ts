9cb5ce6918f3a9b9d8bde6ff3cc8ddc940dcf2b9
Bilyana Ninova <bilyananinova@gmail.com>
2022-04-01 22:13:09 +0300

    create 404 page

--- brandy-shop/src/app/app.module.ts
+++ brandy-shop/src/app/app.module.ts
@@ -20,12 +20,14 @@ import { UserRoutingModule } from './user/user-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home-component/home.component';
 import { HomeCardComponent } from './home/home-card/home-card.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HomeCardComponent
+    HomeCardComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
